fcb09f4775731bfc049bad301d9c8ae7935ec9b8
Andy <andy.ten@tom.com>
2020-02-21 23:56:10 +0800

    将原src/目录下的文件移动到src/js目录下

---
+++ src/js/index2.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:多入口文件
+ * @Author: andy.ten@tom.com
+ * @Date: 2020-02-20 13:52:08
+ * @LastEditors: andy.ten@tom.com
+ * @LastEditTime: 2020-02-21 15:16:37
+ * @Version: 1.0.0
+ */
+function index2() {
+  const element = document.createElement('div');
+  element.innerHTML = '我是第二个入口文件';
+  console.log('我是第二个入口文件');
+  return element;
+}
+
+document.getElementById('root').appendChild(index2());
