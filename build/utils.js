489ecaa4a3f850a5d536ad400dbfb04ca2514adf
Andy <andy.ten@tom.com>
2020-02-21 15:20:05 +0800

    配置文件升级

--- build/utils.js
+++ build/utils.js
@@ -3,7 +3,7 @@
  * @Author: andy.ten@tom.com
  * @Date: 2020-02-20 15:10:56
  * @LastEditors: andy.ten@tom.com
- * @LastEditTime: 2020-02-21 00:05:14
+ * @LastEditTime: 2020-02-21 13:38:28
  * @Version: 1.0.0
  */
 'use strict';
@@ -11,21 +11,21 @@ const path = require('path');
 const config = require('../config');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 引入分离css插件
 
-//拼接路径
+// 拼接路径
 exports.assetsPath = function(_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory;
-  //path.posix.join是path.join的一种兼容性写法，它的作用是路径的拼接，这里返回的是"static/_path"
+  // path.posix.join是path.join的一种兼容性写法，它的作用是路径的拼接，这里返回的是"static/_path"
   return path.posix.join(assetsSubDirectory, _path);
 };
 
-//添加绝对路径
+// 添加绝对路径
 exports.resolve = function(_path) {
   return path.join(__dirname, '..', _path);
 };
 
-//添加css的相关loader，分离css
+// 添加css的相关loader，分离css
 exports.cssLoaders = function(options) {
   options = options || {};
 
@@ -47,9 +47,9 @@ exports.cssLoaders = function(options) {
 
   // generate loader string to be used with extract text plugin
   function generateLoaders(loader, loaderOptions = {}) {
-    // 加载器的数组
+    // 加载器的数组,判断是否使用前缀postcss
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader];
-
+    // console.log(loaders);
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
@@ -59,8 +59,8 @@ exports.cssLoaders = function(options) {
       });
     }
 
-    // Extract CSS when that option is specified
-    // (which is the case during production build)
+    // 分离css文件配置
+    // (在生产环境中配置)
     if (options.extract) {
       // return ExtractTextPlugin.extract({
       //   use: loaders,
@@ -68,7 +68,7 @@ exports.cssLoaders = function(options) {
       // })
       return [MiniCssExtractPlugin.loader].concat(loaders);
     } else {
-      //return ['vue-style-loader'].concat(loaders);
+      // return ['vue-style-loader'].concat(loaders);
       return ['style-loader'].concat(loaders);
     }
   };
@@ -95,7 +95,7 @@ exports.cssLoaders = function(options) {
 exports.styleLoaders = function(options) {
   const output = [];
   const loaders = exports.cssLoaders(options);
-  //console.log('styleLoaders=>loaders:', loaders);
+  // console.log('styleLoaders=>loaders:', loaders);
   for (const extension in loaders) {
     const loader = loaders[extension];
     output.push({
@@ -103,6 +103,6 @@ exports.styleLoaders = function(options) {
       use: loader
     });
   }
-  //console.log('styleLoaders=>outpus:', output);
+  // console.log('styleLoaders=>outpus:', output);
   return output;
 };
